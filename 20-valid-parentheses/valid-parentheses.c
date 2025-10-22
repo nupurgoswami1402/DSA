04e11b5b86e1a46eb9265a4552612b55890dec72
NUPUR GOSWAMI <nupgos07@gmail.com>
2025-10-23 00:07:03 +0530

    Time: 0 ms (100.00%) | Memory: 8.5 MB (23.40%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <string.h>
+
+bool isValid(char * s){
+    char stack[10000];
+    int top = -1;
+
+    for (int i = 0; s[i] != '\0'; i++) {
+        char c = s[i];
+
+        if (c == '(' || c == '{' || c == '[') {
+            stack[++top] = c; // push
+        } else {
+            if (top == -1) return false; // stack empty
+
+            char open = stack[top--]; // pop
+            if ((c == ')' && open != '(') ||
+                (c == '}' && open != '{') ||
+                (c == ']' && open != '['))
+                return false;
+        }
+    }
+
+    return top == -1; // stack should be empty
+}
