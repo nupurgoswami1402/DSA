33068764feb7916a1724335f965e3b9ba509d4cd
NUPUR GOSWAMI <nupgos07@gmail.com>
2025-11-01 15:08:20 +0530

    Time: 5 ms (8.73%) | Memory: 46.5 MB (8.92%) - LeetSync

---
+++ 860-design-circular-queue/design-circular-queue.java
@@ -0,0 +1,46 @@
+class MyCircularQueue {
+    private int[] data;
+    private int head;
+    private int tail;
+    private int size;
+
+    public MyCircularQueue(int k) {
+        data = new int[k];
+        head = 0;
+        tail = -1;
+        size = 0;
+    }
+
+    public boolean enQueue(int value) {
+        if (isFull()) return false;
+        tail = (tail + 1) % data.length;
+        data[tail] = value;
+        size++;
+        return true;
+    }
+
+    public boolean deQueue() {
+        if (isEmpty()) return false;
+        head = (head + 1) % data.length;
+        size--;
+        return true;
+    }
+
+    public int Front() {
+        if (isEmpty()) return -1;
+        return data[head];
+    }
+
+    public int Rear() {
+        if (isEmpty()) return -1;
+        return data[tail];
+    }
+
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    public boolean isFull() {
+        return size == data.length;
+    }
+}
