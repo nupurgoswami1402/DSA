b731527782adae40c1f96e6c76e198469abe622d
NUPUR GOSWAMI <nupgos07@gmail.com>
2026-02-03 20:15:15 +0530

    Time: 24 ms (26.47%) | Memory: 76.9 MB (80.09%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.java
+++ 217-contains-duplicate/contains-duplicate.java
@@ -2,15 +2,16 @@ import java.util.HashSet;
 
 class Solution {
     public boolean containsDuplicate(int[] nums) {
-        HashSet<Integer> seen = new HashSet<>();
-
-        for (int num : nums) {
-            if (seen.contains(num)) {
-                return true;  // Duplicate found
+        Arrays.sort(nums);
+        for(int i =1;i<nums.length;i++){
+            if(nums[i]==nums[i-1]){
+                return true;
             }
-            seen.add(num);
+           
         }
-
-        return false;  // No duplicates
+         return false;
     }
-}
+
+ }
+ 
+        
